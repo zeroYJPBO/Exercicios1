1fe2b243d7116eb161a91caab92ca8068eab396d
João Paulo Oliveira <80533903+zeroYJPBO@users.noreply.github.com>
2022-10-30 22:12:16 -0300

    Add files via upload

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Exercício do URI Online Judge/beecrowd resolvido por João Paulo Bezerra Oliveira
+ * Estudante do curso de Desenvolvimento de Sistemas 4.0 no SENAI Dr. Celso Charuri (Curitiba)
+ * Curitiba, 30 de outubro de 2022
+*/
+
+string[] vetD1 = Console.ReadLine().Split(' ');
+
+string[] vetD2 = Console.ReadLine().Split(' ');
+
+double x1 = double.Parse(vetD1[0]);
+double y1 = double.Parse(vetD1[1]);
+
+double x2 = double.Parse(vetD2[0]);
+double y2 = double.Parse(vetD2[1]);
+
+double calc = Math.Sqrt(Math.Pow(x2 - x1, 2.0) + Math.Pow(y2 - y1, 2.0));
+
+Console.WriteLine(calc.ToString("F4"));
---
+++ Exercicio16/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Exercício do URI Online Judge/beecrowd resolvido por João Paulo Bezerra Oliveira
+ * Estudante do curso de Desenvolvimento de Sistemas 4.0 no SENAI Dr. Celso Charuri (Curitiba)
+ * Curitiba, 30 de outubro de 2022
+*/
+
+double carroX = 1;
+
+double carroY = 2;
+
+int distancia = int.Parse(Console.ReadLine());
+
+double calc1 = carroY * distancia;
+
+Console.WriteLine(calc1 + " minutos");
