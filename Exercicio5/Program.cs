0764bfa3606bdea3811ebffe021b23fb801b05eb
João Paulo Oliveira <80533903+zeroYJPBO@users.noreply.github.com>
2022-10-30 22:07:39 -0300

    Add files via upload

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Exercício do URI Online Judge/beecrowd resolvido por João Paulo Bezerra Oliveira
+ * Estudante do curso de Desenvolvimento de Sistemas 4.0 no SENAI Dr. Celso Charuri (Curitiba)
+ * Curitiba, 30 de outubro de 2022
+*/
+
+double A = double.Parse(Console.ReadLine());
+
+double B = double.Parse(Console.ReadLine());
+
+double calc = ((A * 3.5) + (B * 7.5)) / 11;
+
+Console.WriteLine("MEDIA = " + calc.ToString("F5"));
+
---
+++ Exercicio6/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Exercício do URI Online Judge/beecrowd resolvido por João Paulo Bezerra Oliveira
+ * Estudante do curso de Desenvolvimento de Sistemas 4.0 no SENAI Dr. Celso Charuri (Curitiba)
+ * Curitiba, 30 de outubro de 2022
+*/
+
+double A = double.Parse(Console.ReadLine());
+
+double B = double.Parse(Console.ReadLine());
+
+double C = double.Parse(Console.ReadLine());
+
+double calc = ((A * 2) + (B * 3) + (C * 5)) / 10;
+
+Console.WriteLine("MEDIA = " + calc.ToString("F1"));
