016fa69ba11b3b76549a63e7b0a330b85ce82c50
João Paulo Oliveira <80533903+zeroYJPBO@users.noreply.github.com>
2022-10-30 22:21:24 -0300

    Add files via upload

---
+++ Exercicio17/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Exercício do URI Online Judge/beecrowd resolvido por João Paulo Bezerra Oliveira
+ * Estudante do curso de Desenvolvimento de Sistemas 4.0 no SENAI Dr. Celso Charuri (Curitiba)
+ * Curitiba, 30 de outubro de 2022
+*/
+
+int tempoGasto = int.Parse(Console.ReadLine());
+
+int velMedia = int.Parse(Console.ReadLine());
+
+double progAuto = 12;
+
+double calc1 = (tempoGasto * velMedia) / progAuto;
+
+Console.WriteLine(calc1.ToString("F3"));
---
+++ Exercicio20/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Exercício do URI Online Judge/beecrowd resolvido por João Paulo Bezerra Oliveira
+ * Estudante do curso de Desenvolvimento de Sistemas 4.0 no SENAI Dr. Celso Charuri (Curitiba)
+ * Curitiba, 30 de outubro de 2022
+*/
+
+uint valor = uint.Parse(Console.ReadLine());
+
+uint anos, meses, dias, resto;
+
+anos = valor / 365;
+
+meses = (valor % 365) / 30;
+
+dias = (valor % 365) % 30;
+
+Console.WriteLine(anos + " ano(s)");
+Console.WriteLine(meses + " mes(es)");
+Console.WriteLine(dias + " dia(s)");
